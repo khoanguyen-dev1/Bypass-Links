8b422cad3a03c15e3d8d9f0298f41f7bb6cc551a
khoanguyen-dev1 <kn949892@gmail.com>
2025-01-19 12:24:42 +0700

    Update Banana.user.js

--- Banana.user.js
+++ Banana.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         UwU Bypass
+// @name         UwU Bypass ( Banana )
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  Bypass Banana
