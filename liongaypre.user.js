6d724bd07eb33ec72757f5cde20b0ab0ebc12e49
khoanguyen-dev1 <kn949892@gmail.com>
2025-03-07 21:21:44 +0700

    Create liongaypre.user.js

---
+++ liongaypre.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Bypass UwU
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Bypass = Anh ngầu ( by : UwU ) 
+// @author       Bạn
+// @match        https://flux.li/*
+// @match        https://spdmteam.com/*
+// @match        https://mobile.codex.lol/*
+// @match        https://loot-links.com/*
+// @match        https://linkvertise.com/*
+// @require      https://raw.githubusercontent.com/khoanguyen-dev1/Bypass-Links/main/aaaaaa.js
+// @grant        none
+// ==/UserScript==
